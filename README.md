6397d22de54d1fa253e9e225832d2fd3ce267ec4
VHSgunzo <wosganzo@gmail.com>
2022-07-19 19:45:24 +0300

    Release

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # vkteams_appimage
-AppImage for official desktop application for the VK Teams messaging service
+* AppImage for official desktop application for the VK Teams messaging service
+## To get started:
+* **Download the latest revision**
+```
+git clone https://github.com/VHSgunzo/vkteams_appimage.git
+```
+* **Switch to tool's directory**
+```
+cd vkteams_appimage
+```
+* **Build AppImage**
+```
+./build.sh
+```
+## **AppImage will be located in the build directory**
---
+++ build.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# machine's architecture
+export ARCH=x86_64
+echo "Machine's architecture: $ARCH"
+
+# get the missing tools if necessary
+if [ ! -f appimagetool-$ARCH.AppImage ]
+  then
+      echo "Get the missing tools..."
+      curl -L -o appimagetool-$ARCH.AppImage \
+      https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-$ARCH.AppImage
+      chmod a+x appimagetool-$ARCH.AppImage
+fi
+
+# get sources binary if necessary
+[ ! -f vkteams.tar.xz ] && echo "Get sources binary..." && \
+curl -L -o vkteams.tar.xz https://vkteams-www.hb.bizmrg.com/linux/x64/vkteams.tar.xz
+
+# prepare source AppDir
+if [ -f vkteams.tar.xz ]
+  then
+      echo "Prepare source AppDir..."
+      rm -rvf src && mkdir src
+      tar -xvf vkteams.tar.xz -C src
+      cp -vf com.vk.teams.png src/
+      cp -vf com.vk.teams.desktop src/
+      cp -vf AppRun src/
+      chmod a+x src/AppRun
+      (cd src && ln -sf com.vk.teams.png .DirIcon)
+      cp -rvf usr src/
+fi
+
+# the building
+if [[ -f appimagetool-$ARCH.AppImage && -d src ]]
+  then
+      echo "Building AppImage..."
+      rm -rvf build && mkdir build
+      (cd build && ../appimagetool-$ARCH.AppImage ../src)
+fi
